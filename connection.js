77871d1c26ca2afb13dc4686d026fa99ac3f9a4d
Conscientious Technologies <167194750+CT-Dev-User@users.noreply.github.com>
2025-08-13 15:01:02 +0530

    Update connection.js2

--- connection.js
+++ connection.js
@@ -28,7 +28,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const conn =  mongoose.connect(process.env.MONGODB_URL).then((res)=>{
+export const db =  mongoose.connect(process.env.MONGODB_URL).then((res)=>{
     console.log("DB Connected Successfully..!!")
 }) 
 
